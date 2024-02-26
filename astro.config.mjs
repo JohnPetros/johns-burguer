b14098ce77f201f3adc4c3825da4e10bd55a58c5
johnpetros <joaopedro.nc@outlook.com>
2024-02-26 18:33:58 -0300

    🔧 chore: add astro config

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,4 +1,9 @@
 import { defineConfig } from 'astro/config';
+import biome from "astro-biome";
+import tailwind from "@astrojs/tailwind";
+import react from "@astrojs/react";
 
 // https://astro.build/config
-export default defineConfig({});
+export default defineConfig({
+  integrations: [biome(), tailwind(), react()]
+});
