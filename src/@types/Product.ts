6c115d9a172bbdd340c6a51c2ae7c243587c568d
johnpetros <joaopedro.nc@outlook.com>
2024-02-28 18:07:59 -0300

    🏷️ type: add Cateogory

--- src/@types/Product.ts
+++ src/@types/Product.ts
@@ -1,6 +1,10 @@
+import type { Category } from './Category'
+
 export type Product = {
   id: string
+  name: string
   image: string
   description: string
   price: number
+  category: Category
 }
