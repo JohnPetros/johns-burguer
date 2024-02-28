9b25ddf7bafc5c2ebe7e1697d01b9fb8befc65d7
johnpetros <joaopedro.nc@outlook.com>
2024-02-28 18:07:06 -0300

    🔧 chore: add tailwind breakpoints

--- tailwind.config.mjs
+++ tailwind.config.mjs
@@ -1,8 +1,15 @@
+import { breakpoints } from './src/styles/breakpoints'
+
 /** @type {import('tailwindcss').Config} */
 export default {
-	content: ['./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}'],
-	theme: {
-		extend: {},
+	content: [
+		'./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}',
+		'./node_modules/preline/preline.js',
+	],
+	extend: {
+		screens: breakpoints,
 	},
-	plugins: [],
+	plugins: [
+		require('preline/plugin'),
+	],
 }
