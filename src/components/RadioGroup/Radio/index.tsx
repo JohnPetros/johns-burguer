fcf1470c5440abe698e17c0062a83fd580343c3d
johnpetros <joaopedro.nc@outlook.com>
2024-02-29 17:40:47 -0300

    ✨ feat (ui): add RadioGroup component

---
+++ src/components/RadioGroup/Radio/index.tsx
@@ -0,0 +1,46 @@
+import { RadioGroup } from '@headlessui/react'
+
+import { twMerge } from 'tailwind-merge'
+import { Icon } from '../../Icon'
+import type { RadioProps } from './types/RadioProps'
+
+export function Radio({ value, label, description }: RadioProps) {
+  return (
+    <RadioGroup.Option value={value}>
+      {({ checked }) => (
+        <div
+          className={twMerge(
+            'flex items-center justify-between shadow-md rounded-md p-4 cursor-pointer min-h-16',
+            checked ? 'bg-gray-800' : 'bg-tranparent'
+          )}
+        >
+          <div className='space-y-1'>
+            <RadioGroup.Label
+              className={twMerge(
+                'font-semibold text-md',
+                checked ? 'text-orange-500' : 'text-gray-800'
+              )}
+            >
+              {label}
+            </RadioGroup.Label>
+            {description && (
+              <RadioGroup.Description
+                className={twMerge(
+                  'font-semibold text-sm',
+                  checked ? 'text-gray-100' : 'text-gray-400'
+                )}
+              >
+                {description}
+              </RadioGroup.Description>
+            )}
+          </div>
+          {checked && (
+            <span className='rounded-full grid place-content-center bg-orange-500 w-6 h-6'>
+              <Icon value='check' size={16} className='text-gray-100' />
+            </span>
+          )}
+        </div>
+      )}
+    </RadioGroup.Option>
+  )
+}
---
+++ src/components/RadioGroup/index.tsx
@@ -0,0 +1,30 @@
+import { RadioGroup as Group } from '@headlessui/react'
+import type { ReactNode } from 'react'
+
+type RadioGroupProps = {
+  children: ReactNode
+  defaultValue: string
+  name: string
+  label: string
+  onChange?: (name: string, value: string) => void
+}
+
+export function RadioGroup({
+  children,
+  defaultValue,
+  label,
+  name,
+  onChange,
+}: RadioGroupProps) {
+  console.log(name, { defaultValue })
+  return (
+    <Group
+      onChange={(newValue) => (onChange ? onChange(name, newValue) : null)}
+      defaultValue={defaultValue}
+      name={name}
+    >
+      <Group.Label className='text-orange-500 font-semibold text-lg'>{label}</Group.Label>
+      <div className='space-y-3 mt-3'>{children}</div>
+    </Group>
+  )
+}
