465f35eed221c3fb335c5359c8d59b9bfbeed979
johnpetros <joaopedro.nc@outlook.com>
2024-03-06 20:40:56 -0300

    ✨ feat (api): add Create order api route

---
+++ src/pages/api/create-order.ts
@@ -0,0 +1,18 @@
+import type { APIRoute } from "astro"
+import type { CartItem } from "../../@types/CartItem"
+
+import { Api } from "../../services/api"
+
+import { calculateTotalCartItemsCost } from "../../utils/helpers/calculateTotalCartItemsCost"
+
+export const POST: APIRoute = async ({ request }) => {
+  const cartItems: CartItem[] = await request.json()
+
+  if (!cartItems || !cartItems.length) return new Response('Cart items are not provided')
+
+  const totalCost = calculateTotalCartItemsCost(cartItems)
+
+  const { checkoutToken, orderId } = await Api().createOrder({ items: cartItems, totalCost })
+
+  return new Response(JSON.stringify({ checkoutToken, orderId }))
+} 
