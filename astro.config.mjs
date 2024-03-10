3694c4055eb0c086c414d4a4ab3601fb8ba0916f
johnpetros <joaopedro.nc@outlook.com>
2024-03-10 13:21:07 -0300

    🔧 chore: add netfly server adapter

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,13 +1,14 @@
-import { defineConfig } from 'astro/config';
-import biome from "astro-biome";
-import tailwind from "@astrojs/tailwind";
-import react from "@astrojs/react";
+import { defineConfig } from 'astro/config'
+import biome from 'astro-biome'
+import tailwind from '@astrojs/tailwind'
+import react from '@astrojs/react'
+import vercel from '@astrojs/vercel/serverless'
 
-import vercel from "@astrojs/vercel/serverless";
+import netlify from '@astrojs/netlify'
 
 // https://astro.build/config
 export default defineConfig({
   output: 'server',
   integrations: [biome(), tailwind(), react()],
-  adapter: vercel()
-});
+  adapter: netlify(),
+})
