1f204da4cb1f5d2119e1b2f89bd4004bb2b98aa9
johnpetros <joaopedro.nc@outlook.com>
2024-03-13 18:11:07 -0300

    🏷️ type: add className prop to Separator component

--- src/components/Separator/index.tsx
+++ src/components/Separator/index.tsx
@@ -1,3 +1,13 @@
-export function Separator() {
-  return <span className='block bg-orange-500 w-full h-[2px] rounded-md' />
+import { type ClassNameValue, twMerge } from 'tailwind-merge'
+
+type SeparatorProps = {
+  className?: ClassNameValue
+}
+
+export function Separator({ className }: SeparatorProps) {
+  return (
+    <span
+      className={twMerge('block bg-orange-500 w-full h-[2px] rounded-md', className)}
+    />
+  )
 }
