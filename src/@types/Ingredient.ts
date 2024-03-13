4ab8ad7aff1dda5aa7c0472ed62f788c1d7b983c
johnpetros <joaopedro.nc@outlook.com>
2024-03-13 18:08:59 -0300

    🏷️ type: add price prop to Ingredient type

--- src/@types/Ingredient.ts
+++ src/@types/Ingredient.ts
@@ -1,10 +1,11 @@
+import type { FoodInfo } from './FoodInfo'
+
 export type IngredientCategory = 'sauce' | 'meat' | 'cheese' | 'vegetable' | 'bread'
 
 export type Ingredient = {
   category: IngredientCategory
   name: string
   image: string
-  kcal: number
-  oz: number
-  preparationTime: number
-}
+  position: number
+  price: number
+} & FoodInfo
