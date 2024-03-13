8aa911e6e20f41241ed4321153d1113af4534687
johnpetros <joaopedro.nc@outlook.com>
2024-03-13 17:38:53 -0300

    🏷️ type: add defaultChecked prop

--- src/components/IngredientsTable/IngredientRow/index.tsx
+++ src/components/IngredientsTable/IngredientRow/index.tsx
@@ -4,9 +4,10 @@ import { useIngredientRow } from './useIngredientRow'
 
 type IngredientRowProps = {
   ingredient: Ingredient
+  defaultChecked: boolean
 }
 
-export function IngredientRow({ ingredient }: IngredientRowProps) {
+export function IngredientRow({ ingredient, defaultChecked }: IngredientRowProps) {
   const { handleIngredientCheck } = useIngredientRow(ingredient)
 
   return (
@@ -14,12 +15,16 @@ export function IngredientRow({ ingredient }: IngredientRowProps) {
       htmlFor={ingredient.name}
       className='flex items-center justify-between cursor-pointer'
     >
-      {ingredient.name}
+      <div className='flex items-center gap-3'>
+        <img src={ingredient.image} alt={ingredient.name} width={32} height={32} />
+        {ingredient.name}
+      </div>
       <div className='grid place-content-center rounded-md border-2 border-gray-800 w-6 h-6 bg-transparent has-[:checked]:bg-orange-500 has-[:checked]:border-gray-50 transition duration-200'>
         <input
           type='checkbox'
           id={ingredient.name}
           className='peer hidden'
+          defaultChecked={defaultChecked}
           onChange={handleIngredientCheck}
         />
         <Icon
